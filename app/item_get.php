eab325f2f84904a2c865267bbc0e11c6b5865865
ashen <hansakagaa@gmail.com>
2024-02-06 09:52:24 +0530

    up

--- app/item_get.php
+++ app/item_get.php
@@ -1,13 +1,13 @@
-<?php 
+<?php
 include("../connect.php");
 date_default_timezone_set("Asia/Colombo");
 
 
-$unit=$_GET['unit'];
+$unit = $_GET['unit'];
 
-if($unit == 1){
+if ($unit == 1) {
 
-    $invo=$_GET['invo'];
+    $invo = $_GET['invo'];
     $type = $_GET['type'];
 
 
@@ -17,37 +17,44 @@ if($unit == 1){
     $result->bindParam(':userid', $date);
     $result->execute();
 
-    for($i=0; $row = $result->fetch(); $i++){
-        $img=$row['img'];
-        if($img==''){ $path='img/rice.png'; }else{ $path='product_img/'.$img;}
-        $ch=0; $pro_id=$row['product_id'];
+    for ($i = 0; $row = $result->fetch(); $i++) {
+        $img = $row['img'];
+        if ($img == '') {
+            $path = 'img/rice.png';
+        } else {
+            $path = 'product_img/' . $img;
+        }
+        $ch = 0;
+        $pro_id = $row['product_id'];
         $result2 = $db->prepare("SELECT * FROM sales_list WHERE  invoice_no=:id AND product_id='$pro_id' ");
         $result2->bindParam(':id', $invo);
         $result2->execute();
-        for($i=0; $row1 = $result2->fetch(); $i++){ $ch=$row1['id'];}
-        if($ch == 0){
-        ?>
-        <div class="col-6 col-sm-4 col-md-4 col-lg-4">
-            <div class="info-box">
-                <span class="head"><?php echo $row['name']; ?></span>
-                <span class="sub-head"><?php echo $row['code']?></span>
-                <div class="img-box">
-                    <img src="<?php echo $path; ?>" alt="">
-                </div>
-                <div class="info-foot">
-                    <span class="price">LKR. <?php echo $row['sell']?></span>
-                    <a class="fav" style="color: rgb(var(--bg-black));" href="product_add.php?id=<?php echo $row['product_id']; ?>&invo=<?php echo $invo; ?>">
-                        <i class="fa-solid fa-cart-plus"></i>
-                    </a>
+        for ($i = 0; $row1 = $result2->fetch(); $i++) {
+            $ch = $row1['id'];
+        }
+        if ($ch == 0) {
+?>
+            <div class="col-6 col-sm-4 col-md-4 col-lg-4">
+                <div class="info-box">
+                    <span class="head"><?php echo $row['name']; ?></span>
+                    <span class="sub-head"><?php echo $row['code'] ?></span>
+                    <div class="img-box">
+                        <img src="<?php echo $path; ?>" alt="">
+                    </div>
+                    <div class="info-foot">
+                        <span class="price">LKR. <?php echo $row['sell'] ?></span>
+                        <a class="fav" style="color: rgb(var(--bg-black));" href="product_add.php?id=<?php echo $row['product_id']; ?>&invo=<?php echo $invo; ?>">
+                            <i class="fa-solid fa-cart-plus"></i>
+                        </a>
+                    </div>
                 </div>
             </div>
-        </div>
 
-    <?php }
+        <?php }
     }
 }
 
-if($unit == 2){
+if ($unit == 2) {
 
     $pro_id = $_GET['id'];
     $invo = $_GET['invo'];
@@ -55,23 +62,46 @@ if($unit == 2){
     $result = $db->prepare("SELECT * FROM use_product WHERE main_product = '$pro_id' ");
     $result->bindParam(':id', $res);
     $result->execute();
-    for($i=0; $row = $result->fetch(); $i++){ 
-        $ch=0; $id = $row['product_id'];
+    for ($i = 0; $row = $result->fetch(); $i++) {
+        $ch = 0;
+        $id = $row['product_id'];
         $result2 = $db->prepare("SELECT * FROM sales_list WHERE invoice_no=:id AND  product_id='$id' ");
         $result2->bindParam(':id', $invo);
         $result2->execute();
-        for($i=0; $row1 = $result2->fetch(); $i++){$ch=$row1['id'];}
-        if($ch == 0){?>
-        
-        <h2><?php echo $row['product_name']; ?></h2>
-        <div class="form-group" style="display: flex;align-items: center;">
-            <label>Qty*</label>
-            <input type="number" id="qty_<?php echo $id; ?>" class="form-input w-50 ms-2" autocomplete="off" required>
-            <input type="hidden" id="id_<?php echo $id; ?>" value="<?php echo $id; ?>">
-            <span class="cate-info active" onclick="sales_add_list('<?php echo $id; ?>')"
-            style="margin-left: 60px;padding: 5px 10px;color: rgb(var(--bg-white));font-weight: 600;font-size: 18px;">Add</span>
-        </div>
-    <?php } 
-    } 
-} ?>
+        for ($i = 0; $row1 = $result2->fetch(); $i++) {
+            $ch = $row1['id'];
+        }
+        if ($ch == 0) { ?>
 
+            <div class="col-12 col-md-6 col-lg-4 record ajk_sdy">
+                <div class="info-box" id="info-<?php echo $row['id']; ?>">
+                    <div class="row w-100">
+                        <div class="col-3">
+                            <div class="inb_img-box">
+                                <img src="img/chicken.png" alt="">
+                            </div>
+                        </div>
+                        <div class="col-9 as_jdk">
+                            <div class="i_n_b">
+                                <span class="head"><?php echo $row['name'] ?></span>
+                            </div>
+                            <div class="info-foot">
+                                <div class="qty-box">
+                                    <input type="number" step=".01" class="qty form-input" id="qty_<?php echo $row['id']; ?>">
+                                    <input type="hidden"  id="pid_<?php echo $row['id']; ?>" value="<?php echo $id; ?>">
+                                </div>
+                                <span class="bin btn" onclick="cartClick('<?php echo $row['id']; ?>')">Add</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="dlt-box d-md-none" id="dlt-<?php echo $row['id']; ?>" value="<?php echo $row['id']; ?>" onclick="sales_add_list(<?php echo $row['id']; ?>)">
+                    <a href="#<?php echo $row['id']; ?>" id="<?php echo $row['id']; ?>" class="nav-link">
+                        <i class="fa-solid fa-cart-plus"></i>
+                    </a>
+                </div>
+            </div>
+
+<?php }
+    }
+} ?>
--- app/product_add.php
+++ app/product_add.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CLOUD ARM</title>
 
-    <?php 
+    <?php
     include("head.php");
     include("../connect.php");
     date_default_timezone_set("Asia/Colombo");
@@ -16,44 +17,61 @@
     $result = $db->prepare("SELECT * FROM product WHERE product_id = '$pro_id' ");
     $result->bindParam(':id', $res);
     $result->execute();
-    for($i=0; $row = $result->fetch(); $i++){
+    for ($i = 0; $row = $result->fetch(); $i++) {
         $name = $row['name'];
     }
 
 
     ?>
 </head>
+
 <body class="bg-light customer" style="overflow-y: scroll;">
 
     <div class="container-fluid bg-none">
         <div class="container-fluid my-4">
-            <h1 class="fs-2 fw-semibold m_had"><span><?php echo $name?> </span> </h1>
+            <h1 class="fs-2 fw-semibold m_had"><span><?php echo $name ?> </span> </h1>
         </div>
     </div>
 
-    <div class="container flex my-3" >
-        <div class="box room-container" id="cat-box"></div>
+    <div class="container-fluid mb-3">
+        <div class="box">
+            <div class="box-body">
+                <div class="row w-lg-100" id="cat-box"></div>
+            </div>
+        </div>
     </div>
 
     <div class="container-fluid mb-3 flex">
-        <a href="index.php" class="cate-info active" 
-        style="width: 90%;justify-content: center;font-size: 25px; color: rgb(var(--bg-white)); font-weight: 600;">Next</a>
+        <a href="index.php" class="cate-info active" style="width: 90%;justify-content: center;font-size: 25px; color: rgb(var(--bg-white)); font-weight: 600;">Next</a>
     </div>
 
-    
+
     <!-- Bootstrap 5.3.2-->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"></script>
     <!-- Jquery 3.7.1 -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 
     <script>
+        function cartClick(id) {
+            let info_box = $('#info-' + id);
+            let dlt_box = $('#dlt-' + id);
+            let qty = $('#qty_' + id);
+            if ($(qty).val() > 0) {
+                for (let i = 0; i < $('.info-box').length; i++) {
+                    $($('.info-box')[i]).css("transform", "translateX(0)");
+                    $($('.dlt-box')[i]).css("transform", "translateX(132%)");
+                }
 
+                $(info_box).css("transform", "translateX(-100px)");
+                $(dlt_box).css("transform", "translateX(0)");
+            }
+        }
 
-        $(document).ready(function(){
+        $(document).ready(function() {
             var xmlhttp;
             if (window.XMLHttpRequest) {
                 xmlhttp = new XMLHttpRequest();
-            } else { 
+            } else {
                 xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
             }
             xmlhttp.onreadystatechange = function() {
@@ -62,19 +80,19 @@
                 }
             }
 
-            xmlhttp.open("GET", "item_get.php?unit=2&invo=<?php echo $invo; ?>&id=<?php echo $pro_id; ?>" , true);
+            xmlhttp.open("GET", "item_get.php?unit=2&invo=<?php echo $invo; ?>&id=<?php echo $pro_id; ?>", true);
             xmlhttp.send();
 
 
         });
 
-        function sales_add_list(i){
-            let p_id = document.getElementById('id_'+i).value;
-            let qty = document.getElementById('qty_'+i).value;
+        function sales_add_list(i) {
+            let p_id = document.getElementById('pid_' + i).value;
+            let qty = document.getElementById('qty_' + i).value;
             var xmlhttp;
             if (window.XMLHttpRequest) {
                 xmlhttp = new XMLHttpRequest();
-            } else { 
+            } else {
                 xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
             }
             xmlhttp.onreadystatechange = function() {
@@ -83,10 +101,11 @@
                 }
             }
 
-            xmlhttp.open("GET", "sales_add.php?id="+p_id+"&invo=<?php echo $invo?>&qty="+ qty +"&pro_id=<?php echo $pro_id; ?>" , true);
+            xmlhttp.open("GET", "sales_add.php?id=" + p_id + "&invo=<?php echo $invo ?>&qty=" + qty + "&pro_id=<?php echo $pro_id; ?>", true);
             xmlhttp.send();
 
         }
     </script>
 </body>
+
 </html>
