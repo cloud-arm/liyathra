026be917e06af89c5d0dbe3a9fadadbcff40a228
ashen <hansakagaa@gmail.com>
2024-02-09 15:00:30 +0530

    up

--- app/head.php
+++ app/head.php
@@ -1,5 +1,3 @@
-
-
 <meta charset="UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
@@ -17,6 +15,8 @@
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
 <!-- Font awesome 6.4.2-->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+<!-- Date Picker -->
+<link rel="stylesheet" href="css/datepik.css">
 <!-- Style -->
 <link rel="stylesheet" href="css/stylev5.css">
 <?php include('auth.php'); ?>
