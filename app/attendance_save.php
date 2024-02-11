343d15755f6ab6103db4ad78d2e0488e519dff85
ashen <hansakagaa@gmail.com>
2024-02-11 13:58:59 +0530

    up attendance

--- app/attendance_save.php
+++ app/attendance_save.php
@@ -5,6 +5,7 @@ include("../connect.php");
 
 $date = date("Y-m-d");
 $time = date('H.i.s');
+$user_id = $_SESSION['SESS_MEMBER_ID'];
 
 $id = $_POST['id'];
 
@@ -15,9 +16,9 @@ for ($i = 0; $row = $result->fetch(); $i++) {
     $name = $row['name'];
 }
 
-$sql = "INSERT INTO attendance (emp_id,name,date,time,IN_time,action) VALUES (?,?,?,?,?,?)";
+$sql = "INSERT INTO attendance (emp_id,name,date,time,IN_time,action,user_id) VALUES (?,?,?,?,?,?,?)";
 $q = $db->prepare($sql);
-$q->execute(array($id, $name, $date, $time, $time, 1));
+$q->execute(array($id, $name, $date, $time, $time, 1, $user_id));
 
 
 header("location: index.php");
