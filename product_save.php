f9a47f85b6c713d1ec77cd8358c5e87e6ae44f4d
ashen <hansakagaa@gmail.com>
2024-02-13 14:13:08 +0530

    up product

--- product_save.php
+++ product_save.php
@@ -15,7 +15,7 @@ $amount = $_POST['price'];
 
 $cost = 0;
 $brand = "";
-$brand_name='';
+$brand_name = '';
 $serve_type = 0;
 if ($type == 'Service') {
     $serve_type = $_POST['serve_type'];
@@ -27,7 +27,9 @@ if ($type == 'Product') {
     $result = $db->prepare('SELECT * FROM brand WHERE  id = :id');
     $result->bindParam(':id', $brand);
     $result->execute();
-    for($i=0; $row = $result->fetch(); $i++){ $brand_name=$row['name']; }
+    for ($i = 0; $row = $result->fetch(); $i++) {
+        $brand_name = $row['name'];
+    }
 }
 
 $type_name = '';
@@ -42,33 +44,33 @@ if ($id == 0) {
 
     $sql = "INSERT INTO product (name,job_type,type_name,sell,cost,type,time,brand,brand_id) VALUES (?,?,?,?,?,?,?,?,?)";
     $q = $db->prepare($sql);
-    $q->execute(array($name, $serve_type, $type_name, $amount,$cost, $type, $date,$brand_name,$brand));
+    $q->execute(array($name, $serve_type, $type_name, $amount, $cost, $type, $date, $brand_name, $brand));
 
 
     $result = $db->prepare('SELECT * FROM product ORDER BY product_id DESC LIMIT 1');
     $result->bindParam(':id', $res);
     $result->execute();
-    for($i=0; $row = $result->fetch(); $i++){ $id=$row['product_id']; }
-    
+    for ($i = 0; $row = $result->fetch(); $i++) {
+        $id = $row['product_id'];
+    }
+
     if ($type == 'Service') {
         $sql = 'UPDATE  use_product SET main_product =? WHERE type=? AND main_product=? ';
         $ql = $db->prepare($sql);
-        $ql->execute(array($id,'1', '0'));
+        $ql->execute(array($id, '1', '0'));
     }
 
     if ($type == 'Materials') {
         $sql = 'UPDATE  use_product SET main_product =? WHERE type=? AND main_product=? ';
         $ql = $db->prepare($sql);
-        $ql->execute(array($id,'2', '0'));
+        $ql->execute(array($id, '2', '0'));
     }
-
-
-
 } else {
 
-    $sql = "UPDATE  product SET name = ?, job_type = ?, type_name = ?, sell = ?, type = ? WHERE product_id = ?";
-    $ql = $db->prepare($sql);
-    $ql->execute(array($name, $serve_type, $type_name, $amount, $type, $id));
+
+    // $sql = "UPDATE  product SET name = ?, job_type = ?, type_name = ?, sell = ?, type = ? WHERE product_id = ?";
+    // $ql = $db->prepare($sql);
+    // $ql->execute(array($name, $serve_type, $type_name, $amount, $type, $id));
 }
 
 
