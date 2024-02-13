8952d1945dd796e657a317396c05028c12456380
Eranda Palliyaguru <74122774+eranda-palliyaguru@users.noreply.github.com>
2024-02-13 14:02:47 +0530

    booking wapp

--- app/appointment_save.php
+++ app/appointment_save.php
@@ -40,4 +40,42 @@ $ql = $db->prepare($sql);
 $ql->execute(array($note, $date, $time, $cus, $cus_name, $emp, $user_name, $app_date, $app_time, 'pending', $invo, $order_no, $user_id));
 
 
+if($_SERVER['SERVER_NAME']=='liyathra.colorbiz.org'){
+   
+
+  $curl = curl_init();
+  
+  curl_setopt_array($curl, array(
+    CURLOPT_URL => 'https://chatbiz.net/api/send',
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => '',
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 0,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => 'POST',
+    CURLOPT_POSTFIELDS =>'{
+  "number": "94779252594",
+  "type": "media",
+  "message": "Hi beautiful,
+   Your appointment was booked .Liyathra salon will be delighted serve you on 11/02/2024 at 9.00 am. 
+   See you soon!",
+  "media_url": "https://liyathra.colorbiz.org/main/pages/get/app/img/LIYATHRA BOOKING.png",
+  "instance_id": "65C8624CF2E87",
+  "access_token": "65b8742c1285f"
+  }',
+    CURLOPT_HTTPHEADER => array(
+      'Content-Type: application/json',
+      'Cookie: stackpost_session=efk5hfs38t9mtcfe0lq2laprmohinudj'
+    ),
+  ));
+  
+  $response = curl_exec($curl);
+  
+  curl_close($curl);
+  echo $response;
+  
+}
+
+
 header("location: appointment_service.php?invo=$invo");
