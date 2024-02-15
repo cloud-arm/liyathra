e91b07d010e690f86e48cce04fab1aa3e06b6d2e
ashen <hansakagaa@gmail.com>
2024-02-15 18:24:29 +0530

    up

--- app/sales_rp.php
+++ app/sales_rp.php
@@ -51,67 +51,72 @@
                 </div>
 
                 <p>Today all item sales</p>
-
-                <div class="w-100">
-                    <table class="w-100 mb-2">
-                        <thead>
-                            <tr>
-                                <th>Qty</th>
-                                <th>Cost</th>
-                                <th>Sales</th>
-                                <th>Margin</th>
-                            </tr>
-                        </thead>
-                        <tbody class="border-top">
-                            <?php $total = 0;
-                            $result = $db->prepare($sql1);
-                            $result->bindParam(':id', $res);
-                            $result->execute();
-                            for ($i = 0; $row = $result->fetch(); $i++) { ?>
-                                <tr class="border-top">
-                                    <th><?php echo $row['product_id']; ?></th>
-                                    <th colspan="3"><?php echo $row['name']; ?></th>
-                                </tr>
-                                <tr>
-                                    <td><?php echo $row['sum(sales_list.qty)']; ?></td>
-                                    <td><?php echo $row['sum(sales_list.cost)']; ?></td>
-                                    <td><?php echo $row['sum(sales_list.amount)']  ?></td>
-                                    <td><?php echo $row['sum(sales_list.amount)'] - $row['sum(sales_list.cost)']  ?></td>
-                                    <?php $total += $row['sum(sales_list.amount)'] - $row['sum(sales_list.cost)']; ?>
-                                </tr>
-                            <?php } ?>
-                        </tbody>
-                    </table>
-                </div>
+                <?php $total = 0;
+                $result = $db->prepare($sql1);
+                $result->bindParam(':id', $res);
+                $result->execute();
+                for ($i = 0; $row = $result->fetch(); $i++) {
+                    $total += $row['sum(sales_list.amount)'] - $row['sum(sales_list.cost)'];
+                } ?>
                 <h2 style="font-size: 40px;">Rs.<?php echo number_format($total, 2); ?></h2>
 
             </div>
         </div>
     </div>
 
-    <div class="container room-container">
-        <div class="row" id="room-box">
-
+    <div class="container-fluid mb-3">
+        <div class="box">
+            <div class="box-body d-block">
+                <div class="row ">
+                    <?php $total = 0;
+                    $result = $db->prepare($sql1);
+                    $result->bindParam(':id', $res);
+                    $result->execute();
+                    for ($i = 0; $row = $result->fetch(); $i++) { ?>
+                        <div class="col-12 col-md-6 col-lg-4 record ajk_sdy">
+                            <div class="info-box">
+                                <div class="row w-100">
+                                    <div class="col-3">
+                                        <div class="inb_img-box">
+                                            <img src="img/chicken.png" alt="">
+                                        </div>
+                                    </div>
+                                    <div class="col-9 as_jdk">
+                                        <div class="i_n_b">
+                                            <span class="head"><?php echo $row['name']; ?></span>
+                                        </div>
+                                        <div class="info-foot">
+                                            <div class="qty-box ms-3">
+                                                <span class="price">Qty:<span> <?php echo $row['sum(sales_list.qty)']; ?> </span></span>
+                                            </div>
+                                            <span class="price">LKR. <span><?php echo $row['sum(sales_list.amount)']; ?></span></span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    <?php } ?>
+                </div>
+            </div>
         </div>
-    </div>
 
-    <!-- Bootstrap 5.3.2-->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"></script>
-    <!-- Jquery 3.7.1 -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-    <!-- Date Picker -->
-    <script src="js/datepik.js"></script>
-    <!-- Morris.js charts -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
-    <script src="../../../plugins/morris/morris.min.js"></script>
-    <!-- ChartJS 1.0.1 -->
-    <script src="../../../plugins/chartjs/Chart.min.js"></script>
-    <!-- Sparkline -->
-    <script src="../../../plugins/sparkline/jquery.sparkline.min.js"></script>
+        <!-- Bootstrap 5.3.2-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"></script>
+        <!-- Jquery 3.7.1 -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+        <!-- Date Picker -->
+        <script src="js/datepik.js"></script>
+        <!-- Morris.js charts -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
+        <script src="../../../plugins/morris/morris.min.js"></script>
+        <!-- ChartJS 1.0.1 -->
+        <script src="../../../plugins/chartjs/Chart.min.js"></script>
+        <!-- Sparkline -->
+        <script src="../../../plugins/sparkline/jquery.sparkline.min.js"></script>
 
-    <script>
+        <script>
 
-    </script>
+        </script>
 </body>
 
 </html>
